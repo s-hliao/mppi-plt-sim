15e14a3ee9a2fcaddbdc73963bc323bcd01ad2f3
hzl <hliao62@gatech.edu>
2023-07-07 17:03:55 -0700

    gmm needs fix

--- gmm.py
+++ gmm.py
@@ -72,7 +72,7 @@ def compute_sigma(normalized_expert, k, means, flatten = False, dev = "cpu"):
 
     for i in range(k):
         #print(flat_means[i, :])
-        diff = torch.empty_like(flat_expert[:, :], dtype =torch.float64, device = dev)
+        diff = torch.empty_like(flat_expert[:, :], dtype =torch.float32, device = dev)
         diff = flat_expert[:, :] - flat_means[i, :]
 
     
@@ -104,24 +104,26 @@ def compute_prob(normalized_expert, means, sigma, flatten= False, dev = "cpu"):
     d = torch.linalg.det(sigma).item()
     
     #print(sigma)
-    #print(means)
-    #print(np.linalg.det(sigma.cpu() *3))
+    print(means)
+    #print(np.linalg.det(sigma.cpu()))
 
     denom = np.power(2*np.pi, n/2)*np.power(d, 0.5)
-
     #print(denom)
 
     inv = torch.linalg.pinv(sigma)
 
     exponent_calc = torch.empty(flat_expert.shape[0], device = dev)
-
+    
     diff = flat_expert-flat_means
+
 
     diffS = torch.matmul(diff, inv)
     exponent_calc = -.5 * torch.einsum("ij, ji->i",diffS, diff.t())
-    
+    #print(exponent_calc)
     
     ans = (1/denom) * torch.exp(exponent_calc)
+    #print(ans)
+    # print(ans.sum())
     # print(denom)
     # print(diff)
 
@@ -146,16 +148,19 @@ def E_step(normalized_expert, means, sigma, pi, k, dev = "cpu"):
     #         compute_prob(normalized_expert, means[cluster], sigma[cluster], dev= dev))  
     # print(likelihood)
     # likelihood = likelihood-torch.max(likelihood, dim = 0).values[None,:]
-    print("e step params")
-    print(means)
-    print(sigma)
-    print(pi.sum())
+    # print("e step params")
+    # print(means)
+    # print(sigma)
+    # print(pi.sum())
 
     # recover = torch.exp(likelihood)
-    for cluster in range(k):
-        cluster_prob[cluster, :] = pi[cluster] * compute_prob(normalized_expert, means[cluster], sigma[cluster], dev= dev)
-        print("cluster prob")
-        print(compute_prob(normalized_expert, means[cluster], sigma[cluster], dev= dev))
+    cluster_prob = loglikelihood(normalized_expert, means, sigma, pi, k, dev)
+
+    #print(cluster_prob[:, :10] )
+
+    stabilize = cluster_prob - torch.max(cluster_prob, dim = 0).values[None, :]
+    recover = torch.exp(cluster_prob)
+
     total = torch.sum(cluster_prob, dim= 0)
     resp = cluster_prob/total[None,:]
 
@@ -170,14 +175,14 @@ def M_step(normalized_expert, resp, k, dev = "cpu"):
     new_sigma = torch.empty([k, n, n], device = dev)
     new_pi = torch.empty(k, device = dev)
 
-    print("responsibility")
+    # print("responsibility")
     
-    print(resp)
+    # print(resp)
     
     for cluster in range(k):
         cluster_resp= torch.sum(resp[cluster])
-        print("cluster_resp")
-        print(cluster_resp)
+        # print("cluster_resp")
+        # print(cluster_resp)
 
         new_pi[cluster] = cluster_resp/m
         new_mu[cluster] = torch.sum(resp[cluster,:][:, None]*normalized_expert, dim = 0) * 1/cluster_resp
@@ -187,35 +192,42 @@ def M_step(normalized_expert, resp, k, dev = "cpu"):
         new_sigma[cluster] = torch.matmul(resp[cluster,:].t() * diff.t(), diff)/cluster_resp
     return new_mu, new_sigma, new_pi
 
-
 def loglikelihood(normalized_expert, means, sigma, pi, k, dev = "cpu"):
-
     m = normalized_expert.shape[0]
-    total = torch.zeros([m], dtype = torch.float64, device = dev)
-    
+    total = torch.zeros([k, m], dtype = torch.float32, device = dev)
+    #print("cluster calc")
     for cluster in range(k):
-        total += pi[cluster] * compute_prob(normalized_expert, means[cluster], sigma[cluster], dev = dev)
-    print("likelihood")
-    print(total)
-    ll = torch.log(total)
-    ans = torch.sum(ll)
+        #print(compute_prob(normalized_expert, means[cluster], sigma[cluster], dev = dev))
+        total[cluster, :] = torch.log(pi[cluster]+1e-40) + torch.log(
+            compute_prob(normalized_expert, means[cluster], sigma[cluster], dev = dev)+1e-50)
+
+    return total
+
+def loglikelihood_joint(normalized_expert, means, sigma, pi, k, dev = "cpu"):
+
+    
+    ans = torch.sum(loglikelihood(normalized_expert, means, sigma, pi, k, dev))
 
     return ans
 
-def run_model(expert_rollouts, k=3, iterations = 200):
+def run_model(expert_rollouts, k=3, iterations = 100):
     means, assigments, loss = kmeans.k_means_segment(expert_rollouts, k, 100)
 
     resp = torch.zeros((k, expert_rollouts.shape[0]), dtype = torch.float32, device = torch.device('cuda:0'))
     for i in range(k):
         resp[i, :] = torch.where(assigments==i, 1., 0.)
 
+    speedmean = expert_rollouts[:, :, 0].mean()
+    speedvar = expert_rollouts[:, :, 0].std()
+    steermean = expert_rollouts[:, :, 1].mean()
+    steervar = expert_rollouts[:, :, 1].std()
 
-    normalized_expert = expert_rollouts
-    normalized_expert = torch.empty_like(expert_rollouts, device = torch.device('cuda:0'))
-    
+    print(speedmean, speedvar, steermean, steervar)
 
-    normalized_expert[:, :, 0] = (expert_rollouts[:, :, 0] - 10)
-    normalized_expert[:, :, 1] = (expert_rollouts[:, :, 1] - 0) 
+    normalized_expert = torch.empty_like(expert_rollouts, device = torch.device('cuda:0')) 
+
+    normalized_expert[:, :, 0] = (expert_rollouts[:, :, 0] - speedmean) /speedvar 
+    normalized_expert[:, :, 1] = (expert_rollouts[:, :, 1] - steermean) /steervar
 
     flat_expert, flat_means = flatten_params(normalized_expert, means)
 
@@ -225,22 +237,28 @@ def run_model(expert_rollouts, k=3, iterations = 200):
         flat_means, sigma, pi = M_step(flat_expert, resp, k, torch.device('cuda:0'))
         resp = E_step(flat_expert, flat_means, sigma, pi, k, torch.device('cuda:0'))
 
-        ll = loglikelihood(flat_expert, flat_means, sigma, pi, k, torch.device('cuda:0'))
+        ll = loglikelihood_joint(flat_expert, flat_means, sigma, pi, k, torch.device('cuda:0'))
 
-        
-        print (iter, ll)
+        if iter %10 ==0:
+            print (iter, ll)
+            for i in range(k):
+                torch.set_printoptions(threshold=10000)
+                #print(resp[i, :10] )
     
 
     horizon_length = expert_rollouts.shape[1]
 
-    means[:, :, 0] = (flat_means[:, :horizon_length])*5+5
-    means[:, :, 1] = (flat_means[:, horizon_length:])*4+0
+    means[:, :, 0] = (flat_means[:, :horizon_length])*speedvar/2 + speedmean
+    means[:, :, 1] = (flat_means[:, horizon_length:])*steervar + steermean
 
-    sigma[:, :horizon_length, :] *= 5
-    sigma[:, horizon_length:, :] *= 4
+    sigma[:, :horizon_length, :] *= 1
+    sigma[:, horizon_length:, :] *= 1
 
-    sigma[:, :, :horizon_length] *= 5
-    sigma[:, :, horizon_length:] *= 4
+    sigma[:, :, :horizon_length] *= 1
+    sigma[:, :, horizon_length:] *= 1
+
+    torch.set_printoptions(threshold=1000)
+    print(means)
 
     #print(means.shape)
 
